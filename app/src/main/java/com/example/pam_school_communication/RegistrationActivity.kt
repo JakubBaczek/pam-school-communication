806ca1c525640bec158a49ef09a9074dc53b5624
pawel <pawel.grzymala@student.wat.edu.pl>
2023-06-09 16:33:38 +0200

    User data to Firestore

--- app/src/main/java/com/example/pam_school_communication/RegistrationActivity.kt
+++ app/src/main/java/com/example/pam_school_communication/RegistrationActivity.kt
@@ -8,10 +8,14 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.FirebaseFirestore
+import java.util.Objects
 
 class RegistrationActivity : AppCompatActivity() {
 
     private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var firebaseStore: FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,21 +25,42 @@ class RegistrationActivity : AppCompatActivity() {
         val passwordText: EditText = findViewById(R.id.passwordField)
         val retypePasswordText: EditText = findViewById(R.id.retypePassword)
         val alreadyRegisteredText: TextView = findViewById(R.id.RegisteredTextView)
+        val nameText :EditText = findViewById(R.id.nameField)
+        val secondNameText :EditText = findViewById(R.id.secondNameField)
+
 
         firebaseAuth = FirebaseAuth.getInstance()
+        firebaseStore = FirebaseFirestore.getInstance()
+
 
         registrationButton.setOnClickListener() {
             val email = emailText.text.toString()
             val password = passwordText.text.toString()
             val retypedPassword = retypePasswordText.text.toString()
 
+            //sprawdzenie czy pola nie są puste
             if (email.isNotEmpty() && password.isNotEmpty() && retypedPassword.isNotEmpty()) {
-
+            //sprawdzenie czy hasła się zgadzają
                 if (password == retypedPassword) {
-                    firebaseAuth.createUserWithEmailAndPassword(email, password)
+                    firebaseAuth.createUserWithEmailAndPassword(email, password) //tworzenie użytkownika
                         .addOnCompleteListener {
                             if (it.isSuccessful) {
-                                val intent = Intent(this, MainActivity::class.java)
+                                val user: FirebaseUser? = firebaseAuth.currentUser
+                                val df = user?.let { it1 ->
+                                    firebaseStore.collection("Users").document(
+                                        it1.uid)
+                                }
+                               val userInfo :HashMap<String, Any> = HashMap()
+                                userInfo["Name"] = nameText.text.toString()         //zapis danych użytkownika do fireStore
+                                userInfo["SecondName"] = secondNameText.text.toString()
+                                userInfo["email"] = email
+                                userInfo["IsTeacher"] = "0"
+
+                                if (df != null) {
+                                    df.set(userInfo)
+                                }
+
+                                val intent = Intent(this, MainActivity::class.java) //przejscie do logowania
                                 startActivity(intent)
                             } else {
                                 Toast.makeText(this, "${it.exception}", Toast.LENGTH_LONG).show()
